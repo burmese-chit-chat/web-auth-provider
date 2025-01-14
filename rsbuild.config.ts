e6544a927dfc87ce54d5c6c429d7972ffd0feabf
khine-dev <khine.dev.9417@gmail.com>
2025-01-14 09:01:49 +0700

    micro frontend setup

---
+++ rsbuild.config.ts
@@ -0,0 +1,30 @@
+import { defineConfig } from '@rsbuild/core';
+import { pluginReact } from '@rsbuild/plugin-react';
+import { pluginModuleFederation } from '@module-federation/rsbuild-plugin';
+
+export default defineConfig({
+  plugins: [
+    pluginReact(),
+    pluginModuleFederation({
+      name: 'web_auth_provider',
+      exposes: {
+        './button': './src/button.tsx',
+      },
+      shared : {
+        react : {
+          singleton: true,
+          eager : true, 
+          requiredVersion: false
+        }, 
+        'react-dom' : {
+          singleton: true,
+          eager : true, 
+          requiredVersion: false
+        }
+      }
+    }),
+  ],
+  server: {
+    port: 3001,
+  },
+});
