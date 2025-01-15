82fb4d075aa44d112c750a94f052fe90e5e25c67
khine-dev <khine.dev.9417@gmail.com>
2025-01-16 02:28:27 +0700

    vercel -2

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
     pluginReact(),
     pluginModuleFederation({
       name: 'web_auth_provider',
+	  filename: 'remoteEntry.js',
       exposes: {
         './button': './src/button.tsx',
       },
